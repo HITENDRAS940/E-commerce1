4861cde2c9c5285433dbcc2968f6c806081a631d
hitendrasingh <hitendras940@gmail.com>
2025-09-23 22:00:15 +0530

    Auth using Jwt cookie and signout endpoint added

--- src/main/java/com/hitendra/ecommerce/controller/AuthController.java
+++ src/main/java/com/hitendra/ecommerce/controller/AuthController.java
@@ -13,8 +13,7 @@ import com.hitendra.ecommerce.security.jwt.JwtUtils;
 import com.hitendra.ecommerce.security.services.UserDetailsImplementation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -23,10 +22,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -71,12 +67,17 @@ public class AuthController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         UserDetailsImplementation userDetails = (UserDetailsImplementation) authentication.getPrincipal();
-        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
         List<String> roles = userDetails.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList());
 
-        return new ResponseEntity<>(new UserInfoResponse(userDetails.getId() , jwtToken, userDetails.getUsername(), roles), HttpStatus.OK);
+        UserInfoResponse response = new UserInfoResponse(userDetails.getId() , userDetails.getUsername(), roles);
+
+        return ResponseEntity.ok().header(
+                    HttpHeaders.SET_COOKIE,
+                    jwtCookie.toString()
+                ).body(response);
     }
 
     @PostMapping("/signup")
@@ -136,5 +137,37 @@ public class AuthController {
         user.setRoles(roles);
         userRepository.save(user);
         return ResponseEntity.ok(new MessageResponse("User registered successfully"));
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<UserInfoResponse> getUserDetails(Authentication authentication) {
+
+        UserDetailsImplementation userDetails = (UserDetailsImplementation) authentication.getPrincipal();
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        UserInfoResponse response = new UserInfoResponse(userDetails.getId() , userDetails.getUsername(), roles);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/username")
+    public String getUsername(Authentication authentication) {
+        if(authentication!=null)
+            return authentication.getName();
+        else
+            return "";
+    }
+
+    @PostMapping("/signout")
+    public ResponseEntity<?> signoutUser() {
+        ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
+
+        return ResponseEntity.ok().header(
+                HttpHeaders.SET_COOKIE,
+                cookie.toString()
+        ).body(new MessageResponse("You have been signed out."));
     }
 }
--- src/main/java/com/hitendra/ecommerce/security/jwt/JwtUtils.java
+++ src/main/java/com/hitendra/ecommerce/security/jwt/JwtUtils.java
@@ -1,17 +1,21 @@
 package com.hitendra.ecommerce.security.jwt;
 
+import com.hitendra.ecommerce.security.services.UserDetailsImplementation;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
@@ -23,18 +27,36 @@ public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
     @Value("${spring.app.jwtExpirationMs}")
     private long jwtExpirationMs;
+
     @Value("${spring.app.jwtSecret}")
     private String jwtSecret;
 
-    //Generating JWT from header
-     public String getJwtFromHeader(HttpServletRequest request) {
-         String bearerToken = request.getHeader("Authorization");
-         logger.debug("Authorization header: {}", bearerToken);
-         if(bearerToken!=null && bearerToken.startsWith("Bearer ")) {
-             return bearerToken.substring(7);
-         }
-         return null;
-     }
+    @Value("${spring.ecom.app.jwtCookieName}")
+    private String jwtCookie;
+
+    public String getJwtFromCookies(HttpServletRequest request) {
+        Cookie cookie = WebUtils.getCookie(request, jwtCookie);
+        if(cookie!=null) {
+            return cookie.getValue();
+        } else {
+            return null;
+        }
+    }
+
+    public ResponseCookie generateJwtCookie(UserDetailsImplementation userPrincipal) {
+        String jwt = generateTokenFromUsername(userPrincipal);
+        return ResponseCookie.from(jwtCookie, jwt)
+                .path("/api")
+                .maxAge(24*60*60)
+                .httpOnly(false)
+                .build();
+    }
+
+    public ResponseCookie getCleanJwtCookie() {
+        return ResponseCookie.from(jwtCookie, null)
+                .path("/api")
+                .build();
+    }
 
      //Generating token from username
     public String generateTokenFromUsername(UserDetails userDetails) {
